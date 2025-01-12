85353554f5acad93137590124d892cf9fd71851d
Shreyash <hello@webdevcaptain.com>
2025-01-12 16:53:32 +0000

    Add decrypt file logic and test with a sample file

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,15 @@ use std::{
 };
 
 fn main() {
+    let encrypted_file_path = "sample-file.txt.encrypted";
+
     encrypt_file(
         "sample-file.txt",
+        "password12345678password12345678".as_bytes(),
+    );
+
+    decrypt_file(
+        encrypted_file_path,
         "password12345678password12345678".as_bytes(),
     );
 }
@@ -29,6 +36,23 @@ fn encrypt_file(file_path: &str, key: &[u8]) {
     output_file
         .write_all(&cipher_text)
         .expect("Failed to write encrypted data to the output file");
+}
 
-    println!("IV: {:?}", iv);
+fn decrypt_file(file_path: &str, key: &[u8]) {
+    let encrypted_content = fs::read(file_path).expect("Failed to read the encrypted file");
+
+    let iv = [0u8; 16];
+
+    let cipher = Aes256Cbc::new_from_slices(key, &iv).expect("Failed to create AES cipher");
+
+    let decrypted_content = cipher
+        .decrypt_vec(&encrypted_content)
+        .expect("Failed to decrypt data");
+
+    let mut output_file =
+        File::create(format!("{}.decrypted", file_path)).expect("Failed to create the output file");
+
+    output_file
+        .write_all(&decrypted_content)
+        .expect("Failed to write decrypted data to the output file");
 }
