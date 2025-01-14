084cbe86ad46d7ac36ae1c5ac0543c7f8ee9c6a0
Shreyash <hello@webdevcaptain.com>
2025-01-14 06:40:23 +0000

    Test for folder encryption and decryption

--- encryptify-lib/tests/encryptor_decryptor_tests.rs
+++ encryptify-lib/tests/encryptor_decryptor_tests.rs
@@ -1,9 +1,10 @@
 use std::{
     fs::{self, File},
     io::Write,
+    path::Path,
 };
 
-use encryptify_lib::{decrypt_file, encrypt_file};
+use encryptify_lib::{decrypt_file, decrypt_folder, encrypt_file, encrypt_folder};
 
 // Utility functions
 fn setup_test_file(file_path: &str, content: &[u8]) {
@@ -14,7 +15,14 @@ fn setup_test_file(file_path: &str, content: &[u8]) {
 
 fn cleanup_files(file_paths: &[&str]) {
     for file_path in file_paths {
-        fs::remove_file(file_path).expect("Failed to remove the test file");
+        // Check if folder
+        if Path::new(file_path).is_dir() {
+            fs::remove_dir_all(file_path).expect("Failed to remove the test folder");
+        } else {
+            // Delete the file
+
+            fs::remove_file(file_path).expect("Failed to remove the test file");
+        }
     }
 }
 
@@ -42,4 +50,51 @@ fn test_encrypt_and_decrypt_file() {
     assert_eq!(decrypted_content, content);
 
     cleanup_files(&[filepath, &encrypted_filepath, &decrypted_filepath]);
+}
+
+#[test]
+fn test_encrypt_decrypt_folder() {
+    let folder_path = "test_folder";
+    let key = [0u8; 32]; // 32 bytes key
+    let file1_path = format!("{}/file1.txt", folder_path);
+    let file2_path = format!("{}/file2.txt", folder_path);
+    let content1 = b"Encryptify CLI is awesome! :) ";
+    let content2 = b"Encryptify Library is awesome! :) ";
+
+    // Check if folder already exists and remove it if it does
+    if fs::metadata(&folder_path).is_ok() {
+        fs::remove_dir_all(&folder_path).expect("Failed to remove the test folder");
+    }
+
+    // Create the folder and setup the test files
+    fs::create_dir(&folder_path).expect("Failed to create the test folder");
+    setup_test_file(&file1_path, content1);
+    setup_test_file(&file2_path, content2);
+
+    // Test folder encryption
+    encrypt_folder(&folder_path, &key);
+    assert!(fs::metadata(format!("{}.zip.encrypted", folder_path)).is_ok());
+
+    // Delete the original folder to ensure that decrypt_folder creates a new one
+    fs::remove_dir_all(&folder_path).expect("Failed to remove the test folder");
+
+    decrypt_folder(&format!("{}.zip.encrypted", folder_path), &key);
+
+    let output_folder = folder_path;
+    assert!(fs::metadata(&output_folder).is_ok());
+
+    let file_content1 = fs::read(&file1_path).expect("Failed to read the file1");
+    assert_eq!(file_content1, content1);
+
+    let file_content2 = fs::read(&file2_path).expect("Failed to read the file2");
+    assert_eq!(file_content2, content2);
+
+    // assert!(false);
+
+    cleanup_files(&[
+        &file1_path,
+        &file2_path,
+        output_folder,
+        &format!("{}.zip.encrypted", folder_path),
+    ]);
 }
