9c8c8991d089427eb97163b38bf4992bbad11a74
Shreyash <hello@webdevcaptain.com>
2025-01-16 09:10:09 +0000

    feat(cli): add CLI for encryption and decryption
    This completes the v1 of Encryptify library and cli.

--- encryptify-cli/src/main.rs
+++ encryptify-cli/src/main.rs
@@ -1,14 +1,52 @@
-use encryptify_lib::{decrypt_file, encrypt_file, encrypt_folder};
+use clap::{Arg, Command};
+use std::path::Path;
+
+use encryptify_lib::{decrypt_file, decrypt_folder, encrypt_file, encrypt_folder};
 
 fn main() {
-    let key = "password12345678password12345678".as_bytes();
-    let encrypted_file_path = "sample-file.txt.encrypted";
+    let matches =
+        Command::new("Encryptify")
+            .version("1.0.0")
+            .author("WebDevCaptain")
+            .about("Encrypt and Decrypt files & folders with Rust 🔥")
+            .arg(
+                Arg::new("mode")
+                    .short('m')
+                    .long("mode")
+                    .value_parser(["encrypt", "decrypt"])
+                    .required(true)
+                    .help("Mode: encrypt or decrypt"),
+            )
+            .arg(
+                Arg::new("path")
+                    .short('p')
+                    .long("path")
+                    .required(true)
+                    .help("Path to file or folder"),
+            )
+            .arg(
+                Arg::new("key").short('k').long("key").required(true).help(
+                    "Encryption key (32 characters long). Eg: helloworld123456helloworld123456",
+                ),
+            )
+            .get_matches();
 
-    encrypt_file("sample-file.txt", key);
+    let mode = matches.get_one::<String>("mode").unwrap();
+    let path = matches.get_one::<String>("path").unwrap();
+    let key = matches.get_one::<String>("key").unwrap();
+    let key_bytes = key.as_bytes();
 
-    decrypt_file(encrypted_file_path, key);
-
-    encrypt_folder("vault", key);
-
-    // decrypt_folder("vault.zip.encrypted", key);
+    if mode == "encrypt" {
+        if Path::new(path).is_dir() {
+            encrypt_folder(path, key_bytes);
+        } else {
+            encrypt_file(path, key_bytes);
+        }
+    } else if mode == "decrypt" {
+        if path.ends_with(".zip.encrypted") {
+            decrypt_folder(path, key_bytes);
+        } else {
+            decrypt_file(path, key_bytes);
+        }
+    }
 }
